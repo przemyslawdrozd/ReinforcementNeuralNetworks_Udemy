d5e40d764bf3f6c558d923e1b60cab5144c82f21
przemo06133 <przemo06133@gmail.com>
2019-11-07 20:14:09 +0100

    Constants added

--- src/com/company/markov_process_value_iteration/Constants.java
+++ src/com/company/markov_process_value_iteration/Constants.java
@@ -1,4 +1,24 @@
 package com.company.markov_process_value_iteration;
 
 public class Constants {
+
+    private Constants(){
+    }
+
+    // reward in non-terminal states (used to initialize r[][])
+    public static double STATE_REWARD = 0;
+    // gamma discount factor: how to deal with future rewards
+    public static double GAMMA = 0.99;
+    // want to make the system stochastic: with 80% probability we go where we want
+    public static double ACTION_PROB = 0.8;
+    // with 10 % probability we go in another direction (left or right)
+    public static double ACTION_MISS_PROB = 0.1;
+    // number of iteration
+    public static int NUM_OF_ITER = 100;
+    // to measure the difference |V(actual) - V(optimal)| ~ convergence criterion
+    public static double EPSILON = 1e-7;
+    // broad`s height
+    public static int NUM_ROWS = 3;
+    // broad`s width
+    public static int NUM_COLUMNS = 4;
 }
