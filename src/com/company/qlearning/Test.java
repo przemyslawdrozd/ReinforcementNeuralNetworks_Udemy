c80d9d3971a93cbad089802c8dead80b2c390c8c
przemo06133 <przemo06133@gmail.com>
2019-11-16 17:27:58 +0100

    run and test

---
+++ src/com/company/qlearning/Test.java
@@ -0,0 +1,12 @@
+package com.company.qlearning;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        QLearning qLearning = new QLearning();
+        qLearning.run();
+        qLearning.showResult();
+        qLearning.showPolicy();
+    }
+}
